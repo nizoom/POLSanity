7a29684a51090df62d03237afb2b3ec95a9f02d0
Nissim Ram <nissimram1812@gmail.com>
2022-10-18 17:48:05 -0400

    added validation rule

--- schemas/parentChapter.js
+++ schemas/parentChapter.js
@@ -7,6 +7,7 @@ export default {
       name: "chapterNumber",
       title: "Chapter Number",
       type: "number",
+      validation: (Rule) => Rule.required(),
     },
   ],
 };
--- schemas/subChapter.js
+++ schemas/subChapter.js
@@ -8,11 +8,13 @@ export default {
       type: "reference",
       title: "Parent Chapter",
       to: [{ type: "parentChapter" }],
+      validation: (Rule) => Rule.required(),
     },
     {
       name: "name",
       type: "string",
       title: "Sub Chapter Name",
+      validation: (Rule) => Rule.required(),
     },
   ],
 };
